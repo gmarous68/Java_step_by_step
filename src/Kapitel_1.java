1dfac7db7fbb4dd16ef4afba9d3be9ca08f715c4
Gerald Marous <gmarous@hotmail.com>
2025-08-23 19:50:08 +0200

    kapitel 1 - done

--- src/Kapitel_1.java
+++ src/Kapitel_1.java
@@ -3,19 +3,22 @@ import java.util.Calendar;
 
 public class Kapitel_1 {
     public static void printMyNameAndNumber(String fName, int tlfNo) {
+        // Övning 1.6
         System.out.println("Namn: " + fName);
         System.out.println("Telefonnummer: " + tlfNo);
         System.out.println("*********************************");
 
+        // Övning 1.8
         System.out.println("Namn: " + fName + "\nTelefonnummer: " + tlfNo + "\nAdress: Hemma i Stockholm!");
         System.out.println("*********************************");
 
+        // Övning 1.9
         JOptionPane.showMessageDialog(null, fName);
         JOptionPane.showMessageDialog(null, tlfNo);
 
+        // Övning 1.10
         String text = "Namn: " + fName + "\nTelefonnummer: " + tlfNo + "\nAdress: Hemma i Stockholm!";
         JOptionPane.showMessageDialog(null, text);
-
     }
 
     public static void printOtherStuff() {
@@ -26,6 +29,7 @@ public class Kapitel_1 {
         System.out.println(System.getProperty("user.name"));
         System.out.println("*********************************");
 
+        // Övning 1.7
         System.out.println("Hej! Välkommen " + System.getProperty("user.name") + ", du användar " +
                 "operativ systemet " + System.getProperty("os.name"));
         System.out.println("*********************************");
