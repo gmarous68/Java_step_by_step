87f78cc2b1e2c50825f77b0d00ae16aeff00a408
Gerald Marous <gmarous@hotmail.com>
2025-08-25 12:58:41 +0200

    kapitel 3 - init

---
+++ src/Kapitel_3.java
@@ -0,0 +1,5 @@
+public class Kapitel_3 {
+    public static void App() {
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        Kapitel_3.App();
 
         //Kapitel_2.App();
 
