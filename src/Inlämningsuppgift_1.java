9ce47706ce73133fc1fce28d282998249a752158
Gerald Marous <gmarous@hotmail.com>
2025-09-07 19:09:06 +0200

    Inlämning - changes

--- src/Inlämningsuppgift_1.java
+++ src/Inlämningsuppgift_1.java
@@ -6,7 +6,7 @@ import java.util.UnknownFormatConversionException;
 
 public class Inlämningsuppgift_1 {
     public static void main(String[] args) {
-        del_1();
+//        del_1();
         del_2();
     }
 
@@ -22,7 +22,7 @@ public class Inlämningsuppgift_1 {
         while (tryAgain) {
             try {
                 double width = Double.parseDouble(JOptionPane.showInputDialog("Tast in bredd i (meter) på fönstret: "));
-                double hight = Double.parseDouble(JOptionPane.showInputDialog("Tast in höjd (meter) på fönstret: "));
+                double height = Double.parseDouble(JOptionPane.showInputDialog("Tast in höjd (meter) på fönstret: "));
                 double priceSqrMeters =
                         Double.parseDouble(JOptionPane.showInputDialog("Tast in priset per kvadratmeterglas (kr/m²): "));
 
@@ -30,20 +30,21 @@ public class Inlämningsuppgift_1 {
                 // Otherwise program triggers catch statement and tryAgain is still true.
                 tryAgain = false;
 
-                double CostWithoutDiscount = width * hight * priceSqrMeters;
-                int dicount = (CostWithoutDiscount > 5000) ? 25 : (CostWithoutDiscount > 2500) ? 10 : 5;
-                double CostWithDiscount = CostWithoutDiscount - (CostWithoutDiscount * dicount / 100);
+                double CostWithoutDiscount = width * height * priceSqrMeters;
+                int discount = (CostWithoutDiscount > 5000) ? 25 : (CostWithoutDiscount > 2500) ? 10 : 5;
+                double CostWithDiscount = CostWithoutDiscount - (CostWithoutDiscount * discount / 100);
 
                 String result = "Bredd: " + String.format("%.2f", width) + " meter\n" +
-                        "Höjd: " + String.format("%.2f", hight) + " meter\n" +
+                        "Höjd: " + String.format("%.2f", height) + " meter\n" +
                         "Pris per kvadratmeter: " + String.format("%.2f", priceSqrMeters) + " kr\n" +
                         "Ursprunglig kostnad:  " + String.format("%.2f", CostWithoutDiscount) + " kr\n" +
-                        "Rabatt: " + dicount + "%\n" +
+                        "Rabatt: " + discount + "%\n" +
                         "Slutkostnad: " + String.format("%.2f", CostWithDiscount) + " kr";
                 JOptionPane.showMessageDialog(null, result);
 
             } catch (NullPointerException e) {
                 JOptionPane.showMessageDialog(null, "Cancelled operation!");
+                System.exit(0);
             } catch (NumberFormatException | UnknownFormatConversionException | InputMismatchException e) {
                 JOptionPane.showMessageDialog(null, "Wrong format!");
             } catch (NoSuchElementException e) {
@@ -56,45 +57,37 @@ public class Inlämningsuppgift_1 {
         /**
          * Skapa en enkel miniräknare som beräknar de fyra räknesätten
          */
-        String op1 = "", op2 = "";
-        char operator = ' ';
         int option = -1;
+        String op1, op2;
+        char operator;
 
-        //try/catch - if cancel or x pressed, or zero division/parse errors
-        try {
-            do {
+        do {
+            operator = ' ';
+            op1 = "";
+            op2 = "";
+
+            //try/catch - if cancel or x pressed, or zero division/parse errors
+            try {
                 String output = "Tast in två operander med en operator i mellan som beskrevet nedanför\n"
                         + "\"Giltiga uttryck: 1+2 , -200-100 , 2*5.5 , 10/2\"";
                 String input = JOptionPane.showInputDialog(output);
 
-                // check for empty string
-                if (input.isEmpty()) {
-                    JOptionPane.showMessageDialog(null, "String is empty!");
-                    return;
+                if (!checkIsValid(input)) {
+                    option = JOptionPane.YES_OPTION;
+                    continue;
                 }
 
-                // Replace comma with dot, and remove all whitespaces
-                input = input.replace(",", ".");
-                input = input.replace(" ", "");
-
-                // Check for illegal operator, to many and 0 operators
-                if (!checkIsValidOperators(input))
-                    return;
-
-                // if checkIsValidString = true, Build operators & operand
                 for (int i = 0; i < input.length(); i++) {
                     // string starts with minus - allowed and not counted in total NoOfOperators
                     if (i == 0 && input.charAt(i) == '-') {
                         op1 += input.charAt(0);
                         continue;
                     }
-
                     // read digits until it reaches operator and assign operand 1
                     if (Character.isDigit(input.charAt(i)) || input.charAt(i) == '.') {
                         op1 += input.charAt(i);
                         continue;
                     }
-
                     // after digit(s) - operator is considered present in index i
                     operator = input.charAt(i);
                     // extract and assume the rest are digits - operand 2
@@ -102,44 +95,49 @@ public class Inlämningsuppgift_1 {
                     break;
                 }
 
-                // convert string values to double if possible in order to compute
-                // otherwise catch statement is triggered
-                double operand1 = Double.parseDouble(op1);
-                double operand2 = Double.parseDouble(op2);
-                double sum = 0;
-
-                // figure out the operator to compute with and calculate sum
-                switch (operator) {
-                    case '+' -> sum = operand1 + operand2;
-                    case '-' -> sum = operand1 - operand2;
-                    case '*' -> sum = operand1 * operand2;
-                    case '/' -> sum = operand1 / operand2;
-                    default -> {
-                        JOptionPane.showMessageDialog(null, "Not able to compute!");
-                        return;
-                    }
+                double sum = calculate(op1, op2, operator);
+                if(sum == -1) {
+                    option = JOptionPane.YES_OPTION;
+                    continue;
                 }
-                String message = "Sum of " + operand1 + " " + operator + " " + operand2 + " = " + sum;
+
+                // Result
+                String message = "Sum of " + op1 + " " + operator + " " + op2 + " = "
+                        + String.format("%.2f", sum);
                 JOptionPane.showMessageDialog(null, message);
 
                 // if yes - start over again
                 option = JOptionPane.showConfirmDialog(null, "Nytt forsök? ");
-            } while (option == JOptionPane.YES_OPTION);
-
-        } catch (NullPointerException e) {
-            // if pressed cancel or x
-            JOptionPane.showMessageDialog(null, "Cancelled operation!");
-        } catch (NumberFormatException e) {
-            // if alpha characters in string to number parse
-            JOptionPane.showMessageDialog(null, "Not able to parse to number!!");
-        } catch (ArithmeticException e) {
-            // if divided by 0
-            JOptionPane.showMessageDialog(null, "Division by 0 not allowed!");
-        }
+                if(option != 0) {
+                    JOptionPane.showMessageDialog(null, "Cancelled operation!");
+                    System.exit(0);
+                }
+            } catch (NullPointerException e) {
+                // if pressed cancel or x
+                JOptionPane.showMessageDialog(null, "Cancelled operation!");
+                System.exit(0);
+            } catch (NumberFormatException e) {
+                // if alpha characters in string to number parse
+                JOptionPane.showMessageDialog(null, "Not able to parse to number!!");
+            } catch (ArithmeticException e) {
+                // if divided by 0
+                JOptionPane.showMessageDialog(null, "Division by 0 not allowed!");
+            }
+        } while (option == JOptionPane.YES_OPTION);
     }
 
-    private static boolean checkIsValidOperators(String input) {
+
+    private static boolean checkIsValid(String input) {
         int NoOfOperators = 0;
+
+        if (input.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "String is empty!");
+            return false;
+        }
+
+        // Replace comma with dot, and remove all whitespaces
+        input = input.replace(",", ".");
+        input = input.replace(" ", "");
 
         // Modulus not allowed
         if (input.contains("%")) {
@@ -147,22 +145,54 @@ public class Inlämningsuppgift_1 {
             return false;
         }
 
-        // count the number of operators - only 1 allowed
         for (int i = 0; i < input.length(); i++) {
             // first character might be '-' = is allowed. Not counted in total
             if (i == 0 && input.charAt(i) == '-')
                 continue;
 
+            if(Character.isDigit(input.charAt(i)) || input.charAt(i) == '.')
+                continue;
+
             switch (input.charAt(i)) {
-                case '*': case '/': case '-': case '+': NoOfOperators++;
+                case '*':
+                case '/':
+                case '-':
+                case '+':
+                    NoOfOperators++;
             }
         }
-
-        // Number of operator(s) found in string is 0 or > 1
         if (NoOfOperators != 1) {
             JOptionPane.showMessageDialog(null, "No of operator is 0 or exceeds 1!");
             return false;
         }
         return true;
+    }
+
+
+    private static double calculate(String op1, String op2, char operator) {
+        double sum = -1;
+
+        // if operand are empty
+        if(op1.isEmpty() || op2.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Operand 1 or operand 2 is not set!");
+            return sum;
+        }
+
+        double operand1 = Double.parseDouble(op1);
+        double operand2 = Double.parseDouble(op2);
+
+        switch (operator) {
+            case '+' -> sum = operand1 + operand2;
+            case '-' -> sum = operand1 - operand2;
+            case '*' -> sum = operand1 * operand2;
+            case '/' -> {
+                if (operand2 == 0) throw new ArithmeticException();
+                // if ArithmeticException not triggered in line above - normal sum operation with double
+                sum = operand1 / operand2;
+            }
+            default ->
+                JOptionPane.showMessageDialog(null, "Not able to compute!");
+        }
+        return sum;
     }
 }
